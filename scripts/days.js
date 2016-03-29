ed653f44ac2c4cc32fef5081c4f29814763ff788
Matthew Ginnard <matthew.ginnard@gmail.com>
2016-03-28 23:23:24 -0700

    update days.js.

--- scripts/days.js
+++ scripts/days.js
@@ -1,16 +1,26 @@
-// Day checker.
-
+// Create a variable called today, that maps to a
 var day = new Date();
 var today = day.getDay();
 
-
 module.exports = function(robot) {
-    var dayPattern = /day/;
+    var dayPattern = "/do we have class today?/i";
     robot.respond(dayPattern, function(msg) {
         if (today = 1) {
-            msg.reply("It's Monday!");
+            msg.reply("Today's Monday. Class is at 6:30.");
+        } else if (today = 3) {
+            msg.reply("Today's Wednesday. You have class at 6:30.");
         } else {
-            msg.reply("Nope, it's not Monday.");
+            if (today = 0) {
+                msg.reply("Today's Sunday. The JS class only meets on Mondays and Wednesdays.");
+            } else if (today = 2) {
+                msg.reply("It's Tuesday. You have class on Mondays and Wednesdays.");
+            } else if (today = 4) {
+                msg.reply("It's Thursday. No class today.");
+            } else if (today = 5) {
+                msg.reply("It's Friday. Your next class is on Monday.");
+            } else if (today = 6) {
+                msg.reply("It's Saturday. Enjoy the weekend! Class meets Mondays and Wednesdays.");
+            };
         };
     });
 };
--- scripts/weather.js
+++ scripts/weather.js
@@ -3,3 +3,13 @@
 // API KEY: 86fb953a79eb4050f91fe514981d02e0
 
 // Set up a function that takes a zip code and fetches the current weather.
+
+var zipcodePattern = /weather [0-9]{5}/;
+
+module.exports = function(robot) {
+    robot.respond(zipcodePattern, function(msg) {
+        var zipcode = new RegExp(zipcodePattern);
+        return console.log(zipcode.toString());
+        msg.reply("Here's the weather for zipcode");
+    });
+};
