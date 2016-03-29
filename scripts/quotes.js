a850dead3e315af118976195721d422b505413b2
Matthew Ginnard <matthew.ginnard@gmail.com>
2016-03-28 18:23:14 -0700

    comments in quotes.js

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,6 +1,4 @@
-// Returns a single motivation from an array when given the "give me a quote" or "inspire me" command.
-// If the command "add quote '<quote>'" is given, the quote is added to the array.
-
+// Give bot an array of quotes to use at the beginning.
 var quotes = [
     "Everything is going to be 200 OK.",
     "You look nice today!",
@@ -8,10 +6,20 @@ var quotes = [
     "You did well today! Did you know that?"
 ];
 
+// Return a single motivation from an array when given 
+// the "give me a quote" or "inspire me" command.
 module.exports = function(robot) {
+    // Establish a pattern to match the quote command.
     var quotePattern = /quote/;
     robot.respond(quotePattern, function(msg) {
+        // Pull a random quote from the quotes array.
+        // Note: Because the quote should be random *every time*,
+        // we need to re-assign randomQuote each time, and the 
+        // function should be placed inside the .respond() method.
         var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+        // Pass the random quote into the reply.
         msg.reply(randomQuote);
     });
 };
+
+// TODO: If the command "add quote '<quote>'" is given, the quote is added to the array.
